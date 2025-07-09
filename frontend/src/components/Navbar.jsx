462df3ea5751f7720dd99a5d75f037807de17a83
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-09 13:00:10 +0300

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-3 group">
            <div className="w-10 h-10 bg-gradient-to-r from-[#00468B] to-[#FFB800] rounded-lg flex items-center justify-center group-hover:shadow-lg transition-shadow duration-300">
-  <img src="/images/ieee-logo.png" alt="IEEE Logo" className="w-6 h-6 object-contain" />
+  <img src="/images/ieee-logo.png" alt="IEEE Logo" className="w-full h-full object-contain" />
 </div>
             <div className="hidden sm:block">
               <h1 className="text-lg font-bold text-[#00468B] group-hover:text-[#FFB800] transition-colors duration-300">
