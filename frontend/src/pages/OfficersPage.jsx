5141efcf997a26532f45f6f1390ce68e2a27d975
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-10 14:58:22 +0300

    Navbar

--- frontend/src/pages/OfficersPage.jsx
+++ frontend/src/pages/OfficersPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 import Officers from '../components/Officers';
 import Footer from '../components/Footer';
 
 const OfficersPage = () => {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <Officers />
       <Footer />
     </div>
