9584e495676cb5c616dba6c1a43efcd63439d7a2
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-10 22:44:23 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,30 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
-// Pages
 import HomePage from "./pages/HomePage";
 import OfficersPage from "./pages/OfficersPage";
 import OpeningDay from "./components/OpeningDay";
 import AboutIEEE from "./components/AboutIEEE";
-import JoinJSYP from "./pages/JoinJSYP"; // <-- 1. Import the new page
+
+// Corrected the import path to point to the 'components' folder
+import JoinJSYP from "./components/JoinJSYP"; 
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar /> {/* Navbar appears on all pages */}
+      <Navbar />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/officers" element={<OfficersPage />} />
           <Route path="/opening-day" element={<OpeningDay />} />
           <Route path="/about-ieee" element={<AboutIEEE />} />
-          <Route path="/join-jsyp" element={<JoinJSYP />} /> {/* <-- 2. Add the new route */}
+          <Route path="/join-jsyp" element={<JoinJSYP />} />
         </Routes>
       </main>
-      <Footer /> {/* Footer appears on all pages */}
+      <Footer />
     </BrowserRouter>
   );
 }
