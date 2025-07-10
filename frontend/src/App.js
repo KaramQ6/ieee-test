27fd27819d1666ee812a0db67cee4bf747b87375
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-10 14:51:49 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar"; // استورد الـ Navbar
+import Footer from "./components/Footer"; // إذا عندك Footer ضيفه كمان
+
 import HomePage from "./pages/HomePage";
 import OfficersPage from "./pages/OfficersPage";
-
-// Corrected import to point to the component file in the 'components' directory
 import OpeningDay from "./components/OpeningDay"; 
 import AboutIEEE from "./components/AboutIEEE"; 
 
-
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/officers" element={<OfficersPage />} />
-          
-          {/* Using the correct component name 'OpeningDay' */}
-          <Route path="/opening-day" element={<OpeningDay />} />
-          
-          <Route path="/about-ieee" element={<AboutIEEE />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar /> {/* صار يظهر بكل الصفحات */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/officers" element={<OfficersPage />} />
+        <Route path="/opening-day" element={<OpeningDay />} />
+        <Route path="/about-ieee" element={<AboutIEEE />} />
+      </Routes>
+      {/* <Footer /> ممكن تضيف الفوتر كمان تحت */}
+    </BrowserRouter>
   );
 }
 
