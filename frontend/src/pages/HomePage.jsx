e66b152447694068bb3a44cfb182debe59950979
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-10 23:12:12 +0300

    edit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Timeline from '../components/Timeline';
-import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Hero />
       <Timeline />
-      <Footer />
     </div>
   );
 };
--- frontend/src/pages/OfficersPage.jsx
+++ frontend/src/pages/OfficersPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Officers from '../components/Officers';
-import Footer from '../components/Footer';
 
 const OfficersPage = () => {
   return (
     <div className="min-h-screen">
       <Officers />
-      <Footer />
     </div>
   );
 };
