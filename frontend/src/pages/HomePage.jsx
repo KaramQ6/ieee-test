6c825aadec15914105827f29dd5b72a17af5dcab
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-22 03:03:17 +0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import Timeline from '../components/Timeline';
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Hero />
-      <Timeline />
     </div>
   );
 };
